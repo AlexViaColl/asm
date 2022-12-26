43be038d1e3fea364dae0258277210398e122588
Alex Via <alexviacoll@gmail.com>
2022-12-26 13:06:59 -0800

    Implement FLDZ (opcode: d9 ee)

--- asm.py
+++ asm.py
@@ -1084,6 +1084,9 @@ def disassemble(raw, state=None):
                 elif raw[1] == 0xe0:
                     state['eip'] += 2
                     return f'FCHS'
+                elif raw[1] == 0xee:
+                    state['eip'] += 2
+                    return f'FLDZ'
                 elif raw[1] == 0xf8:
                     state['eip'] += 2
                     return f'FPREM'
