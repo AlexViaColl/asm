957b0aa6e95da9591963a7a067ce1312e0541392
Alex Via <alexviacoll@gmail.com>
2022-12-26 19:10:05 -0800

    Implement FISUBR dword-integer (opcode: da, nnn: 101)

--- asm.py
+++ asm.py
@@ -1290,7 +1290,9 @@ def disassemble(raw, state=None):
                     state['eip'] += 2
                     return f'FISUB DWORD PTR {addr}'
                 elif nnn == 0b101:
-                    pass
+                    addr = modrm_addressing(raw[1], raw[2:], state)
+                    state['eip'] += 2
+                    return f'FISUBR DWORD PTR {addr}'
                 elif nnn == 0b110:
                     addr = modrm_addressing(raw[1], raw[2:], state)
                     state['eip'] += 2
