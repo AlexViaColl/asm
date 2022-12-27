4094b4a0588be8af5cc27e55dc8a304275eb9db1
Alex Via <alexviacoll@gmail.com>
2022-12-26 19:09:29 -0800

    Implement FISUB dword-integer (opcode: da, nnn: 100)

--- asm.py
+++ asm.py
@@ -1286,7 +1286,9 @@ def disassemble(raw, state=None):
                     state['eip'] += 2
                     return f'FICOMP DWORD PTR {addr}'
                 elif nnn == 0b100:
-                    pass
+                    addr = modrm_addressing(raw[1], raw[2:], state)
+                    state['eip'] += 2
+                    return f'FISUB DWORD PTR {addr}'
                 elif nnn == 0b101:
                     pass
                 elif nnn == 0b110:
--- test.py
+++ test.py
@@ -634,6 +634,7 @@ if __name__ == '__main__':
         'FIMUL DWORD PTR [esp+0x14]':       b'\xda\x4c\x24\x14',
         'FICOM DWORD PTR [esp+0x14]':       b'\xda\x54\x24\x14',
         'FICOMP DWORD PTR [esp+0x14]':      b'\xda\x5c\x24\x14',
+        'FISUB DWORD PTR [esp+0x14]':       b'\xda\x64\x24\x14',
         'FIDIV DWORD PTR [esp+0x14]':       b'\xda\x74\x24\x14',
 
         'FLD TBYTE PTR ds:0x8f81c8':        b'\xdb\x2d\xc8\x81\x8f\x00',
