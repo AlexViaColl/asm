5a3192c42ca778f1d7aa50f0ffd62f442d7f867c
Alex Via <alexviacoll@gmail.com>
2022-12-26 19:06:56 -0800

    Implement FIMUL dword-integer (opcode: da, nnn: 001)

--- asm.py
+++ asm.py
@@ -1274,7 +1274,9 @@ def disassemble(raw, state=None):
                     state['eip'] += 2
                     return f'FIADD DWORD PTR {addr}'
                 elif nnn == 0b001:
-                    pass
+                    addr = modrm_addressing(raw[1], raw[2:], state)
+                    state['eip'] += 2
+                    return f'FIMUL DWORD PTR {addr}'
                 elif nnn == 0b010:
                     pass
                 elif nnn == 0b011:
