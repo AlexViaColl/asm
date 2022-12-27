931ac88f9cbb96e4aaf62ba76dedb25e2a74d824
Alex Via <alexviacoll@gmail.com>
2022-12-27 00:44:30 -0800

    Implement FST (opcode: dd d0-d7)

--- asm.py
+++ asm.py
@@ -1479,6 +1479,9 @@ def disassemble(raw, state=None):
                 if raw[1] >= 0xc0 and raw[1] <= 0xc7:
                     state['eip'] += 2
                     return f'FFREE st({raw[1] - 0xc0})'
+                elif raw[1] >= 0xd0 and raw[1] <= 0xd7:
+                    state['eip'] += 2
+                    return f'FST st({raw[1] - 0xd0})'
                 elif raw[1] >= 0xd8 and raw[1] <= 0xdf:
                     state['eip'] += 2
                     return f'FSTP st({raw[1] - 0xd8})'
--- test.py
+++ test.py
@@ -697,6 +697,7 @@ if __name__ == '__main__':
         'FNSAVE [esi+0x8]':                 b'\xdd\x76\x08',
         'FNSTSW WORD PTR [ebp-0x4]':        b'\xdd\x7d\xfc',
         'FFREE st(0)':                      b'\xdd\xc0',
+        'FST st(0)':                        b'\xdd\xd0',
         'FSTP st(1)':                       b'\xdd\xd9',
 
         'FICOM WORD PTR [edi*2+0x7d14c800]':b'\xde\x14\x7d\x00\xc8\x14\x7d',
