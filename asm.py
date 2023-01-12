7a435171a244483e6d8c0806f105f46ed13a2d0c
Alex Via <alexviacoll@gmail.com>
2023-01-11 21:23:51 -0800

    MOV r/m8,r8 and MOV r/m8, imm8 with disp32

--- asm.py
+++ asm.py
@@ -2839,25 +2839,45 @@ def assemble(line, state):
                         src = REGISTERS8.index(tokens[9].value)
                         modrm = 0b10000100 | src << 3
                         sib = 0b00100000 | reg
-                        if disp <= 0x7f:
-                            if reg == REGISTERS.index('esp'):
-                                modrm = 0b01000000 | src << 3 | reg
-                                sib = 0b00100100
-                                return b'\x88' + pack('<B', modrm) + pack('<B', sib) + pack('<B', disp)
+                        if reg == REGISTERS.index('esp'):
+                            if disp <= 0x7f:
+                                if reg == REGISTERS.index('esp'):
+                                    modrm = 0b01000000 | src << 3 | reg
+                                    sib = 0b00100100
+                                    return b'\x88' + pack('<B', modrm) + pack('<B', sib) + pack('<B', disp)
+                                else:
+                                    modrm = 0b01000000 | src << 3 | reg
+                                    return b'\x88' + pack('<B', modrm) + pack('<B', disp)
+                            elif disp <= 0xff:
+                                modrm = 0b10000000 | src << 3 | reg
+                                return b'\x88' + pack('<B', modrm) + pack('<I', disp)
                             else:
-                                modrm = 0b01000000 | src << 3 | reg
-                                return b'\x88' + pack('<B', modrm) + pack('<B', disp)
-                        elif disp <= 0xff:
-                            modrm = 0b10000000 | src << 3 | reg
-                            return b'\x88' + pack('<B', modrm) + pack('<I', disp)
+                                return b'\x88' + pack('<B', modrm) + pack('<B', sib) + pack('<I', disp)
                         else:
-                            return b'\x88' + pack('<B', modrm) + pack('<B', sib) + pack('<I', disp)
+                            if disp <= 0x7f:
+                                if reg == REGISTERS.index('esp'):
+                                    modrm = 0b01000000 | src << 3 | reg
+                                    sib = 0b00100100
+                                    return b'\x88' + pack('<B', modrm) + pack('<B', sib) + pack('<B', disp)
+                                else:
+                                    modrm = 0b01000000 | src << 3 | reg
+                                    return b'\x88' + pack('<B', modrm) + pack('<B', disp)
+                            elif disp <= 0xff:
+                                modrm = 0b10000000 | src << 3 | reg
+                                return b'\x88' + pack('<B', modrm) + pack('<I', disp)
+                            else:
+                                modrm = 0b10000000 | src << 3 | reg
+                                return b'\x88' + pack('<B', modrm) + pack('<I', disp)
                     else:
                         ib = int(tokens[9].value, base=16)
                         if reg == REGISTERS.index('esp'):
-                            modrm = 0b01000000 | reg
                             sib = 0b00100100
-                            return b'\xc6' + pack('<B', modrm) + pack('<B', sib) + pack('<b', disp) + pack('<B', ib)
+                            if disp <= 0x7f:
+                                modrm = 0b01000000 | reg
+                                return b'\xc6' + pack('<B', modrm) + pack('<B', sib) + pack('<b', disp) + pack('<B', ib)
+                            else:
+                                modrm = 0b10000000 | reg
+                                return b'\xc6' + pack('<B', modrm) + pack('<B', sib) + pack('<I', disp) + pack('<B', ib)
                         else:
                             if disp <= 0x7f:
                                 modrm = 0b01000000 | reg
