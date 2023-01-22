6b43d0ecca27cb4341150ebd38fe6af9d6dd621b
Alex Via <alexviacoll@gmail.com>
2023-01-21 16:20:45 -0800

    PSRLW

--- asm.py
+++ asm.py
@@ -4862,6 +4862,11 @@ def assemble(line, state):
         assert tokens[2].value == ','
         ib = int(tokens[3].value, base=16)
         return b'\x0f\x72' + pack('<B', 0xd0 + dst) + pack('<B', ib)
+    elif opcode == 'PSRLW':
+        dst = int(tokens[1].value[-1])
+        assert tokens[2].value == ','
+        ib = int(tokens[3].value, base=16)
+        return b'\x0f\x71' + pack('<B', 0xd0 + dst) + pack('<B', ib)
     elif opcode == 'PSUBD':
         if tokens[1].value in REGISTERSMM:
             dst = REGISTERSMM.index(tokens[1].value)
--- test.py
+++ test.py
@@ -1696,6 +1696,7 @@ def test_assemble():
         ['PSRAW mm0, 0x2',                  b'\x0f\x71\xe0\x02'],
         ['PSRAW mm4, 0x2',                  b'\x0f\x71\xe4\x02'],
         ['PSRLD mm6, 0x12',                 b'\x0f\x72\xd6\x12'],
+        ['PSRLW mm1, 0x2',                  b'\x0f\x71\xd1\x02'],
         ['PSUBD mm1, QWORD PTR [ebp+0x2c]', b'\x0f\xfa\x4d\x2c'],
         ['PSUBD mm0, QWORD PTR [ebp-0x34]', b'\x0f\xfa\x55\xcc'],
         ['PSUBD mm0, QWORD PTR [ebp-0x94]', b'\x0f\xfa\x85\x6c\xff\xff\xff'],
